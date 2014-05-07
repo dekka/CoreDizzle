ba19355f964d83071923b94ffd6af6aeba77b9c6
Reed Sweeney <reed.sweeney@gmail.com>
2014-05-07 09:25:39 -0700

    Initial Commit

---
+++ CoreDizzle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreDizzle
+//
+//  Created by Reed Sweeney on 5/7/14.
+//  Copyright (c) 2014 Reed Sweeney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
